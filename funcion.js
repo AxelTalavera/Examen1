7ffba0164cb8fde3533368efd3b36c7af37b9ced
Axel Talaverl <axell.yahoo.com.es@gmail.com>
2022-04-29 22:57:07 -0600

    Ir

--- funcion.js
+++ funcion.js
@@ -1,22 +1,67 @@
 function Calcular(){
     let primer_valor= document.getElementById("b");
 
-   
+
+    let SB = Number(primer_valor.value);
+    document.getElementById("SB").value = SB
+
     let resultad = Number(primer_valor.value) *0.07;
     document.getElementById("Ins").value = resultad
 
+    let Deduccion = Number(primer_valor.value) -resultad;
+    document.getElementById("sN").value = Deduccion
 
-    let ir = Number(primer_valor.value) *0.15;
-    document.getElementById("IR").value = ir
-
+  
+    let r =Deduccion *12;
+    let re=r-200000;
+    let mul=re*0.20;
+    let ip=mul+15000;
+     irA=ip/12;
+     document.getElementById("IR").value=irA;
+/*
+   if (primer_valor>=100000.01 && primer_valor<=200000){
+         let r =Deduccion *12;
+         let re=r-200000;
+         let mul=re*0.20;
+         let ip=mul+15000;
+          irA=ip/12;
+          document.getElementById("IR").value=irA;
+        
+    } 
     
-    let sn = Number(primer_valor.value) -ir-resultad;
-    document.getElementById("sN").value = sn
+   
+    
+/*
+    if(primer_valor>0 && primer_valor<=100000){
+        let ir = Number(0);
+        
+    }
+else if(primer_valor>=100000.01 && primer_valor<=200000){
+    let primer =salario-100000;
+    primer_valor=primer*0.15;
+    document.getElementById("IR").value=primer_valor/12;
+}
+else if(primer_valor>=200000.01 && primer_valor<=350000){
+    let primer =salario-200000;
+    primer_valor=(primer*0.20)+15000;
+    document.getElementById("IR").value=primer_valor/12;
+}
 
-    let Sd = Number(sn);
-    document.getElementById("SD").value = Sd
+else if(primer_valor>=350000.01 && primer_valor<=500000){
+    let primer =salario-200000;
+    primer_valor=(primer*0.25)+45000;
+    document.getElementById("IR").value=primer_valor/12;
+    
+}
+else{
+  let primer=primer_valor-500000;
+  primer=(primer*0.30)+82500
+  document.getElementById("IR").value=primer;  
+}*/
 
-    let SN = Number(primer_valor.value)*1;
-    document.getElementById("SB").value = SN
+
+let Fin = Number(primer_valor.value) -resultad;
+    document.getElementById("SD").value = Fin
+
 
 }
